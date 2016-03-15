78083d2feb588c7b5354ac0bce42c5ac414ccc64
Lokendra Prajapati <Lokendra2500@users.noreply.github.com>
2016-03-15 22:00:29 +0530

    Updated controller.js

--- view/controller/controller.js
+++ view/controller/controller.js
@@ -1,5 +1,7 @@
 /**
- * Created by Lokendra on 3/15/2016.
+ * Auther: Lokendra Prajapati
+ * Date: March 15, 2016
+ * Purpose: This controller file contains all the methods, that will help to perform CRUD operations in database(MongoDB database).
  */
 
 var contact_app = angular.module('contactApp', []);
@@ -17,29 +19,33 @@ contact_app.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
 
 refresh();
 
-	$scope.addContact = function(){
+	$scope.addContact = function(){				//Add contact information in to database
 		console.log($scope.contact);
-		$http.post('/contactList', $scope.contact).success(function(response){
+		/*
+		* This POST method tells the server for adding the contact into database, 
+		* This method send the request to the server.
+		*/
+		$http.post('/contactList', $scope.contact).success(function(response){ 
 			console.log(response);
 			refresh();
 		});
 	};
 
-	$scope.removeContact = function(id){
+	$scope.removeContact = function(id){		//Remove the contact from database
 		console.log(id);
 		$http.delete('/contactList/' + id).success(function(response){
 			refresh();
 		});
 	};
 
-	$scope.editContact = function(id){
+	$scope.editContact = function(id){		// Edit contact information
 		console.log(id);
 		$http.get('/contactList/' +id).success(function(response){
 			$scope.contact = response;
 		});
 	};
 
-	$scope.updateContact = function(){
+	$scope.updateContact = function(){		// Update the contact information
 		console.log($scope.contact._id);
 		$http.put('/contactList/'+$scope.contact._id, $scope.contact).success(function(response){
 			refresh();
